16e747cc9245082f37b9d368e718d33b8e792112
AbhishekP <abhisheksaralaya2006@gmail.com>
2026-09-21 17:13:03 +0530

    Solved Running sum problem by prefix sum

---
+++ src/DSA/day09/Runningsum.java
@@ -0,0 +1,19 @@
+package DSA.day09;
+
+public class Runningsum {
+
+        public int[] runningSum(int[] nums) {
+
+            int sum = 0;
+
+            int num[] = new int[nums.length];
+
+            for(int i = 0; i < nums.length; i++) {
+                sum += nums[i];
+                num[i] = sum;
+            }
+
+            return num;
+        }
+
+}
