31a926305b0359e78c02d1ca279a00efebb67d40
AbhishekP <abhisheksaralaya2006@gmail.com>
2026-09-22 17:38:59 +0530

    Solved rangesum problem

---
+++ src/DSA/Rangesum.java
@@ -0,0 +1,22 @@
+package DSA;
+
+public class Rangesum {
+        int[] pnum;
+        public NumArray(int[] nums) {
+            pnum=new int[nums.length];
+            pnum[0]=nums[0];
+            for(int i=1;i<nums.length;i++){
+                pnum[i]=pnum[i-1]+nums[i];
+
+            }
+        }
+        public int sumRange(int left, int right) {
+            if(left==0){
+                return pnum[right];
+            }
+            else if(left>0){
+                return pnum[right]-pnum[left-1];
+            }
+            else
+                return -1;
+        }}
